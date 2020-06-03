e941d1b7a57dcf11c18e136618073d9d46c0b44c
Glauber Charles <glauber.charles2012@gmail.com>
2020-06-03 17:09:28 -0300

    Projeto completo de For

---
+++ Exerc1For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exerc1For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia um valor inteiro X(1 <= X <= 1000).Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+            X, se for o caso.*/
+
+            Console.WriteLine("Digite um valor: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
